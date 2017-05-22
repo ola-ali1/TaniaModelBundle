8b8cc9c3a931f88a19cbdfeba675c13b6873010f
Sarah Mostafa <sarah.marzouk@ibtikar.net.sa>
2017-05-22 14:13:28 +0200

    Create tania model bundle

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ibtikar\TaniaModelBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('IbtikarTaniaModelBundle:Default:index.html.twig');
+    }
+}
---
+++ IbtikarTaniaModelBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ibtikar\TaniaModelBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IbtikarTaniaModelBundle extends Bundle
+{
+}
