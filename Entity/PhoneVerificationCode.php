63ea42456aad61b5bdd1d5f03779ef1f3e19ea77
Moemen <moemen.hussein@ibtikar.net.sa>
2017-05-23 11:33:34 +0200

    Updating Tania Model Bundle Fixing use url

--- Entity/PhoneVerificationCode.php
+++ Entity/PhoneVerificationCode.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 class PhoneVerificationCode
 {
 
-    use \Ibtikar\ShareEconomyToolsBundle\Entity\TrackableTrait;
+    use \Ibtikar\TaniaModelBundle\Entity\TrackableTrait;
 
     /**
      * @var integer
