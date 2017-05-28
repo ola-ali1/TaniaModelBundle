964bea08c73dedd9410cb69f52400b6c17022f68
Moemen <moemen.hussein@ibtikar.net.sa>
2017-05-28 11:02:53 +0200

    adding image to item

--- Entity/Item.php
+++ Entity/Item.php
@@ -121,7 +121,7 @@ class Item
     {
         // the absolute directory path where uploaded
         // documents should be saved
-        return __DIR__ . '/../../../../../web/' . $this->getUploadDir();
+        return __DIR__ . '/../../../../web/' . $this->getUploadDir();
     }
 
     protected function getUploadDir()
