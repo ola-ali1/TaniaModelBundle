49875b699289a1f37a20edcadb578d76e7b88c45
Sarah Mostafa <sarah.marzouk@ibtikar.net.sa>
2017-06-01 10:25:57 +0200

    driver entity

--- Entity/Van.php
+++ Entity/Van.php
@@ -3,8 +3,6 @@
 namespace Ibtikar\TaniaModelBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * @ORM\Table(name="van")
