f6993769622908f95d4d89693284cf51a3477780
Moemen <moemen.hussein@ibtikar.net.sa>
2017-06-01 11:35:00 +0200

    adding using uploadfile

--- Entity/Van.php
+++ Entity/Van.php
@@ -2,6 +2,7 @@
 
 namespace Ibtikar\TaniaModelBundle\Entity;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 use Doctrine\ORM\Mapping as ORM;
 
