ad5facd5f6266567ea1a2f6d58ccbf297fc765ac
Sarah Mostafa <sarah.marzouk@ibtikar.net.sa>
2017-06-13 12:02:14 +0200

    change default price column to decimal not string

---
+++ DoctrineMigrations/Version20170613100126.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ibtikar\TaniaModelBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170613100126 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE item CHANGE default_price default_price NUMERIC(10, 2) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE item CHANGE default_price default_price VARCHAR(255) NOT NULL COLLATE utf8mb4_general_ci');
+    }
+}
