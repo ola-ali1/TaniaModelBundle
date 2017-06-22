08fe378eef11de22fa3f509ca8b696efb274d711
Mahmoud mostafa <mahmoud.mostafa@ibtikar.net.sa>
2017-06-22 12:51:33 +0200

    Task 1182 : Dev/ UMS| Add role http://tfs.ibtikar.sa/tfs/Ibtikar-Projects/Tania%20Backend/_workitems?id=1182&triage=true&_a=edit
    Work Done :
    ===========
    All the user story

---
+++ DoctrineMigrations/Version20170621151553.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ibtikar\TaniaModelBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170621151553 extends AbstractMigration
+{
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(25) NOT NULL, permissions LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, version INT DEFAULT 1 NOT NULL, UNIQUE INDEX UNIQ_57698A6A5E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE role');
+    }
+}
---
+++ Entity/Role.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Ibtikar\TaniaModelBundle\Entity;
+
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Role
+ *
+ * @UniqueEntity(fields={"name"})
+ * @ORM\Table(name="role")
+ * @ORM\Entity(repositoryClass="Ibtikar\TaniaModelBundle\Repository\RoleRepository")
+ */
+class Role
+{
+
+    use \Ibtikar\TaniaModelBundle\Entity\TrackableTrait;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank(message="fill_mandatory_field")
+     * @Assert\Length(min = 1, max = 25)
+     * @ORM\Column(name="name", type="string", length=25, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var array
+     *
+     * @Assert\Count(
+     *      min = "1",
+     *      minMessage = "You must have at least 1 Permission"
+     * )
+     * @ORM\Column(name="permissions", type="array")
+     */
+    private $permissions;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Role
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set permissions
+     *
+     * @param array $permissions
+     *
+     * @return Role
+     */
+    public function setPermissions($permissions)
+    {
+        $this->permissions = $permissions;
+
+        return $this;
+    }
+
+    /**
+     * Get permissions
+     *
+     * @return array
+     */
+    public function getPermissions()
+    {
+        return $this->permissions;
+    }
+}
