74142d6bcc38fa055acd20878db3e0266b75c981
Sarah Mostafa <sarah.marzouk@ibtikar.net.sa>
2017-07-27 14:21:14 +0200

    task 2692 &2697: add/edit/list close reasons => http://tfs.ibtikar.eg:8080/tfs/Ibtikar-Projects/Tania%20Backend/_workitems/edit/2692,  http://tfs.ibtikar.eg:8080/tfs/Ibtikar-Projects/Tania%20Backend/_workitems/edit/2697

---
+++ DoctrineMigrations/Version20170727114409.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ibtikar\TaniaModelBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170727114409 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE close_reason (id INT AUTO_INCREMENT NOT NULL, reasonAr VARCHAR(190) NOT NULL, reasonEn VARCHAR(190) NOT NULL, type VARCHAR(30) NOT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, version INT DEFAULT 1 NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE close_reason');
+    }
+}
