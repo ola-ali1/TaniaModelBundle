e943c6ddeab01e14eff0ec3710a4c395697dd6ae
moamen <moamen@Micheal-PC>
2017-07-30 13:07:26 +0200

    Work Done : =========== - assign returned status as current order Remaining Work: =============== - filter order by status

--- Entity/Order.php
+++ Entity/Order.php
@@ -37,7 +37,7 @@ class Order implements PfTransactionInvoiceInterface
     );
 
     public static $statusCategories = array(
-        'current'   => array('placed', 'verified', 'delivering'),
+        'current'   => array('placed', 'verified', 'delivering', 'returned'),
         'past'      => array('delivered', 'closed', 'cancelled')
     );
 
