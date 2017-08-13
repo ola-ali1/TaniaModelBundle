d2e7d81d340adb18917b83501b4afcaafba61984
abdelrahman allam <abdelrahman.allam@ibtikar.net.sa>
2017-08-13 16:29:33 +0200

    847#View list of balance requests

--- Entity/BalanceRequest.php
+++ Entity/BalanceRequest.php
@@ -14,4 +14,70 @@ use Ibtikar\TaniaModelBundle\Entity\Order;
 class BalanceRequest extends Order
 {
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="points", type="integer")
+     * @Assert\Type(type="numeric")
+     */
+    protected $points;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="decimal", precision=10, scale=2, options={"default": 0})
+     * @Assert\Type(type="numeric")
+     */
+    protected $price;
+
+    /**
+     * Set points
+     *
+     * @param string $points
+     *
+     * @return Balance
+     */
+    public function setPoints($points)
+    {
+        $this->points = $points;
+
+        return $this;
+    }
+
+    /**
+     * Get points
+     *
+     * @return string
+     */
+    public function getPoints()
+    {
+        return $this->points;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Balance
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+
 }
