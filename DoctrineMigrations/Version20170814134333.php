298b16f788c7973a8f23a4a2446dd1fd5ad61436
abdelrahman allam <abdelrahman.allam@ibtikar.net.sa>
2017-08-14 16:53:49 +0200

    Task 1724# Dev/ API integration With Tania Backend
    completed
    =========
    Migration Add flag for track user is synced with service

---
+++ DoctrineMigrations/Version20170814134333.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ibtikar\TaniaModelBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170814134333 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+
+        $this->addSql('ALTER TABLE user ADD isSynced TINYINT(1) DEFAULT \'0\'');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    
+        $this->addSql('ALTER TABLE user DROP isSynced');
+    }
+}
