85972f97506817894b6e55b1a19a8e2481dcad63
abdelrahman allam <abdelrahman.allam@ibtikar.net.sa>
2017-08-15 13:51:14 +0200

    Task 1724# Dev/ API integration With Tania Backend
    completed
    =========
    Migration update flag to false for all users

---
+++ DoctrineMigrations/Version20170815093519.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ibtikar\TaniaModelBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170815093519 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("UPDATE `user` SET `isSynced`='false' WHERE `isSynced` = ''");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("UPDATE `user` SET `entityClass`='' WHERE `entityClass` = 'false'");
+
+    }
+}
