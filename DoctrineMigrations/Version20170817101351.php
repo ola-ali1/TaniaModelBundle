e598fb30af1555851666127464489ba3ab2c4f40
abdelrahman allam <abdelrahman.allam@ibtikar.net.sa>
2017-08-17 12:30:05 +0200

    Merge branch 'master' of https://github.com/Ibtikar/Tania-Dashboard
    1530# API integration With Tania Backend
    Completed
    ==========
    add IsAsync to order and migration file
    
    Remaining
    =========
    testing, and test the cronjob

---
+++ DoctrineMigrations/Version20170817101351.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ibtikar\TaniaModelBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170817101351 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `order` ADD isSynced TINYINT(1) DEFAULT \'0\'');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `order` DROP isSynced');
+    }
+}
