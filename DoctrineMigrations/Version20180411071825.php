b9bea2a986862efb18500a2e8a697cc2f423b335
Mahmoud mostafa <mahmoud.mostafa@ibtikar.net.sa>
2018-04-11 10:00:09 +0200

    Task #16938: Development \ Max number of items per order http://tfs.ibtikar.sa/tfs/Ibtikar-Projects/TaniaEverywere/_workitems/edit/16938
    Work Done :
    ===========
    Add migration for the new settings bundle

---
+++ DoctrineMigrations/Version20180411071825.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ibtikar\TaniaModelBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180411071825 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE dmishh_settings (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, value LONGTEXT DEFAULT NULL, owner_id VARCHAR(255) DEFAULT NULL, INDEX name_owner_id_idx (name, owner_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE dmishh_settings');
+    }
+}
