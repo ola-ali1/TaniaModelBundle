0dae2d51158d4d1ed8e325ea70709d98f6231fc7
Mahmoud mostafa <mahmoud.mostafa@ibtikar.net.sa>
2018-04-29 14:50:04 +0200

    Task #17958: Development \ API | Promocode Calculate Receipt http://tfs.ibtikar.sa/tfs/Ibtikar-Projects/TaniaEverywere/_workitems/edit/17958
    Work Done :
    ===========
    Add new functions to update the promocode values

--- Repository/PromoCodeRepository.php
+++ Repository/PromoCodeRepository.php
@@ -22,4 +22,52 @@ class PromoCodeRepository extends EntityRepository
                 ->setMaxResults(1)
                 ->getQuery()->getResult();
     }
+
+    /**
+     * @author Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
+     * @param integer $id
+     * @param double $orderDiscountAmount
+     * @param boolean $usedByUserBefore
+     * @return boolean
+     */
+    public function increasePromoCodeUsage($id, $orderDiscountAmount, $usedByUserBefore = false)
+    {
+        $dql = '
+            UPDATE IbtikarTaniaModelBundle:PromoCode pc
+            SET pc.numberOfUsedTimes = pc.numberOfUsedTimes + 1
+            SET pc.usageTotalAmount = pc.usageTotalAmount + :usageTotalAmount
+            ';
+        if (!$usedByUserBefore) {
+            $dql .= ' SET pc.numberOfUsedByUsers = pc.numberOfUsedByUsers + 1';
+        }
+        $dql .= ' WHERE pc.id = :id';
+        return $this->getEntityManager()->createQuery($dql)
+                ->setParameter('id', $id)
+                ->setParameter('usageTotalAmount', $orderDiscountAmount)
+                ->execute();
+    }
+
+    /**
+     * @author Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
+     * @param integer $id
+     * @param double $orderDiscountAmount
+     * @param boolean $usedByUserBefore
+     * @return boolean
+     */
+    public function decreasePromoCodeUsage($id, $orderDiscountAmount, $usedByUserBefore = false)
+    {
+        $dql = '
+            UPDATE IbtikarTaniaModelBundle:PromoCode pc
+            SET pc.numberOfUsedTimes = pc.numberOfUsedTimes - 1
+            SET pc.usageTotalAmount = pc.usageTotalAmount - :usageTotalAmount
+            ';
+        if (!$usedByUserBefore) {
+            $dql .= ' SET pc.numberOfUsedByUsers = pc.numberOfUsedByUsers - 1';
+        }
+        $dql .= ' WHERE pc.id = :id';
+        return $this->getEntityManager()->createQuery($dql)
+                ->setParameter('id', $id)
+                ->setParameter('usageTotalAmount', $orderDiscountAmount)
+                ->execute();
+    }
 }
