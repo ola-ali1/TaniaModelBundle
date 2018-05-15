1062f1436655c70bbad99b8bbda09c00f495a6dc
khaled <khaled.eleskandarany@ibtikar.net.sa>
2018-05-15 13:52:35 +0200

    Story #000
    Completed:
    ----------
    
    - Modified masajed list

--- Repository/UserAddressRepository.php
+++ Repository/UserAddressRepository.php
@@ -8,18 +8,20 @@ use Ibtikar\TaniaModelBundle\Entity\UserAddress;
 class UserAddressRepository extends EntityRepository
 {
 
-    public function getAllMasajed($page, $limit = 25){
+    public function getAllMasajed($page, $userLat, $userLong, $limit = 25){
 
-        $query = $this->createQueryBuilder('ua')
-            ->select('ua')
-            ->where("ua.type = :masjedType");
+        $query = $this->createQueryBuilder('l')
+            ->select('l')
+            ->where("l.type = :masjedType")
+            ->addSelect('((ACOS(SIN(:lat * PI() / 180) * SIN(l.latitude * PI() / 180) + COS(:lat * PI() / 180) * COS(l.latitude * PI() / 180) * COS((:lng - l.longitude) * PI() / 180)) * 180 / PI()) * 60 * 1.1515) as HIDDEN distance')
+            ->orderBy('distance');
 
         if($page !== 0){
             $query = $query->setMaxResults($limit)
                 ->setFirstResult(($page -1)* $limit);
         }
-        $query = $query->setParameters(array('masjedType'=> UserAddress::TYPE_MASAJED))
-            ->orderBy('ua.id', 'DESC')
+        $query = $query->setParameters(array('masjedType'=> UserAddress::TYPE_MASAJED, 'lat' => $userLat, 'lng' => $userLong))
+            ->orderBy('l.id', 'DESC')
             ->getQuery()
             ->getResult();
 
