4fdebc5d71dcb4a58f2d4e2cdb1c91544e71ee33
khaled <khaled.eleskandarany@ibtikar.net.sa>
2018-05-21 18:12:21 +0200

    Story #000
    Completed:
    ----------
    
    - Updated nana sync data

--- Entity/Order.php
+++ Entity/Order.php
@@ -352,6 +352,68 @@ class Order implements PfTransactionInvoiceInterface
     protected $userAddress;
 
     /**
+     * @var boolean
+     *
+     * @ORM\Column(name="is_nana_synced", type="boolean",  options={"default": 0}, nullable=true)
+     */
+    protected $isNanaSynced;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="nana_sync_data", type="array", nullable=true)
+     */
+    private $nanaSyncData;
+
+    /**
+     * Set nanaSyncData
+     *
+     * @param array $nanaSyncData
+     *
+     * @return array
+     */
+    public function setNanaSyncData($nanaSyncData)
+    {
+        $this->nanaSyncData = $nanaSyncData;
+
+        return $this;
+    }
+
+    /**
+     * Get nanaSyncData
+     *
+     * @return array
+     */
+    public function getNanaSyncData()
+    {
+        return $this->nanaSyncData;
+    }
+
+    /**
+     * Set isNanaSynced
+     *
+     * @param boolean $isNanaSynced
+     *
+     * @return boolean
+     */
+    public function setIsNanaSynced($isNanaSynced)
+    {
+        $this->isNanaSynced = $isNanaSynced;
+
+        return $this;
+    }
+
+    /**
+     * Get $isNanaSynced
+     *
+     * @return boolean
+     */
+    public function getIsNanaSynced()
+    {
+        return $this->isNanaSynced;
+    }
+
+    /**
      * Set promoCode
      *
      * @param \Ibtikar\TaniaModelBundle\Entity\PromoCode $promoCode
