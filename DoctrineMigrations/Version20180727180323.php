f1f8ee1040860c9806d3d5fefe46e700ebd5f121
ahmed.yahia <ahmed.mahmoud@ibtikar.net.sa>
2018-07-26 19:13:38 +0200

    sukaka to sakakah

---
+++ DoctrineMigrations/Version20180727180323.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ibtikar\TaniaModelBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180727180323 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("UPDATE `city` SET name_en = 'Sakakah' WHERE name_en = 'Sukaka' limit 1;");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("UPDATE `city` SET name_en = 'Sukaka' WHERE name_en = 'Sakakah' limit 1;");
+
+    }
+}
